2850b96e135796bf2d78aa00d7bad4bcbb49f5fa
Marcus <40230046+hibernatusMV@users.noreply.github.com>
2019-12-29 11:19:45 +0100

    adding types of steps for speed

--- RedCatFocus.ino
+++ RedCatFocus.ino
@@ -1,8 +1,25 @@
+/* Build a focusing unit for the William Optics RedCat 51
+ * Created by Marcus Vasi
+ * www.eifeltwister.space
+ */
+
+ /* Configure type of steps
+  *            MS1 MS2 MS3
+  * Full step   0   0   0
+  * 1/2 step    1   0   0
+  * 1/4 step    0   1   0
+  * 1/8 step    1   1   0
+  * 1/16 step   1   1   1
+  */
+
 // definition of stepper motor parameter
 #define stepPin 5
 #define dirPin 6
 #define stepsPerRevolution 5
 #define stepDelay 2000
+#define MS1 9
+#define MS2 10
+#define MS3 11
 
 // definition of input pins
 #define pinForward 2
@@ -20,10 +37,18 @@ void setup() {
   // set internal PullUps
   pinMode(pinForward, INPUT_PULLUP);
   pinMode(pinBackward, INPUT_PULLUP);
+  pinMode(MS1, OUTPUT);
+  pinMode(MS2, OUTPUT);
+  pinMode(MS3, OUTPUT);
+
+  // initial settings for type of steps (1/16)
+  digitalWrite(MS1, HIGH);
+  digitalWrite(MS2, HIGH);
+  digitalWrite(MS3, HIGH);
 }
 
 void loop() {
-  // check which button is pressed. Avoid both buttons are pressed simultaneous
+  // check which button is pressed. Avoidto move motor if both buttons are pressed simultaneous
   if (digitalRead(pinForward) == LOW && digitalRead(pinBackward) != LOW)
   {
     Serial.println("Focus forward");
