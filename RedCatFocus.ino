c90855ea5d8f5b920572aa91a7a9d1720d74d12a
Marcus <40230046+hibernatusMV@users.noreply.github.com>
2019-12-30 13:01:15 +0100

    adding IR remote functionality

--- RedCatFocus.ino
+++ RedCatFocus.ino
@@ -12,6 +12,13 @@
   * 1/16 step   1   1   1
   */
 
+#include <IRremote.h>
+
+// configuration for remote control
+int RECV_PIN = 3;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
 // definition of stepper motor parameter
 #define stepPin 6
 #define dirPin 5
@@ -26,11 +33,17 @@
 #define pinForward 2
 #define pinBackward 3
 
+String previousVal = "";
+
 
 void setup() {
   Serial.begin(9600);
   delay(200);
   Serial.println("Serial established");
+
+  // Start the receiver
+  irrecv.enableIRIn();
+  
   // declare motor pins as output
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
@@ -51,29 +64,41 @@ void setup() {
 }
 
 void loop() {
-  // check which button is pressed. Avoidto move motor if both buttons are pressed simultaneous
-  if (digitalRead(pinForward) == LOW && digitalRead(pinBackward) != LOW) {
-    Serial.println("Focus forward");
-    for (int i = 0; i < stepsPerRevolution; i++) {
-      // set the dir pin clockwise
-      digitalWrite(dirPin, HIGH);
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(stepDelay);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(stepDelay);
+  if (irrecv.decode(&results)) {
+    String buttonPress = decodeIR(results.value);
+    if (buttonPress == "REP") {
+      buttonPress = previousVal;
+    } else {
+      previousVal = buttonPress;
     }
-  }
-
-  if (digitalRead(pinBackward) == LOW && digitalRead(pinForward) != LOW) {
-    Serial.println("Focus backward");
-    for (int i = 0; i < stepsPerRevolution; i++) {
-      // set the dir pin counterclockwise
-      digitalWrite(dirPin, LOW);
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(stepDelay);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(stepDelay);
+    
+    // check which button is pressed. Avoidto move motor if both buttons are pressed simultaneous
+    // if (digitalRead(pinForward) == LOW && digitalRead(pinBackward) != LOW) {
+    if (buttonPress == "UP") {
+      Serial.println("Focus forward");
+      for (int i = 0; i < stepsPerRevolution; i++) {
+        // set the dir pin clockwise
+        digitalWrite(dirPin, HIGH);
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(stepDelay);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(stepDelay);
+      }
     }
+  
+    // if (digitalRead(pinBackward) == LOW && digitalRead(pinForward) != LOW) {
+    if (buttonPress == "DOWN") {
+      Serial.println("Focus backward");
+      for (int i = 0; i < stepsPerRevolution; i++) {
+        // set the dir pin counterclockwise
+        digitalWrite(dirPin, LOW);
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(stepDelay);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(stepDelay);
+      }
+    }
+    irrecv.resume(); // Receive the next value
   }
 }
 
@@ -109,5 +134,38 @@ void setMicrostep(int stepType) {
       digitalWrite(MS2, HIGH);
       digitalWrite(MS3, HIGH);
       break;
+  }
+}
+
+String decodeIR (long x) {
+  String hexCode = String(x, HEX);
+  if (hexCode == "fd10ef") {
+      return "DOWN";
+  } else if (hexCode == "fd50af") {
+      return "UP";
+  } else if (hexCode == "fd30cf") {
+      return "0";
+  } else if (hexCode == "fd08f7") {
+      return "1";
+  } else if (hexCode == "fd8877") {
+      return "2";
+  } else if (hexCode == "fd48b7") {
+      return "3";
+  } else if (hexCode == "fd28d7") {
+      return "4";
+  } else if (hexCode == "fda857") {
+      return "5";
+  } else if (hexCode == "fd6897") {
+      return "6";
+  } else if (hexCode == "fd18e7") {
+      return "7";
+  } else if (hexCode == "fd9867") {
+      return "8";
+  } else if (hexCode == "fd58a7") {
+      return "9";
+  } else if (hexCode == "ffffffff") {
+      return "REP";
+  } else {
+    return "NA";
   }
 }
